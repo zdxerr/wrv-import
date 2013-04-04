c926db9edc80875e87c68a0234154e42a41461e1
zdxerr <zdxerr@gmail.com>
2013-04-04 12:44:07 +0200

    Changed result parser init.

--- move.py
+++ move.py
@@ -4,12 +4,14 @@
 import shutil
 
 import find_results
-import test-result-parser
+import test_result_parser
 
 WRV_IMPORT_PATH = r'I:\WRV_Import_CRTI-BTP'
 
 if __name__ == '__main__':
     result_path = r"R:\PE\Testdata\CRTI-Test\ImplSW_RLS_2013-A"
     for result in find_results.results(result_path, 'testbutler'):
+        r = test_result_parser.TestbutlerResult(result_path)
         print result
-        os.makedirs()
+        print r
+        # os.makedirs()
